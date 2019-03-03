c8df0291e3b483ee602191f883fb9f3490a60b09
Ryan Plas <ryan@wordplas.com>
2019-03-03 00:14:44 -0500

    Fix Download URL

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='pyphantomapi',
-    version='0.1.1',
+    version='0.1.2',
     description='Python Wrapper for the Splunk Phantom API',
     url='https://github.com/ryanplasma/pyphantomapi',
-    download_url='https://github.com/ryanplasma/pyphantomapi/archive/0.1.tar.gz',    author='Ryan Plas',
+    download_url='https://github.com/ryanplasma/pyphantomapi/archive/0.1.2.tar.gz',    author='Ryan Plas',
     author_email='ryan@wordplas.com',
     license='MIT',
     packages=['pyphantomapi'],
