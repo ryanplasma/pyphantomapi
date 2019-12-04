e110f67dcaa2c220f7b9483830aed1895553f0b1
Ryan Plas <ryan@wordplas.com>
2019-12-03 22:09:54 -0500

    Add App Query

--- pyphantomapi/models/__init__.py
+++ pyphantomapi/models/__init__.py
@@ -2,5 +2,6 @@ from .phantom.container import Container
 from .phantom.artifact import Artifact
 from .phantom.action_run import ActionRun
 from .phantom.asset import Asset
+from .phantom.app import App
 
-__all__ = ('Container', 'Artifact', 'ActionRun', 'Asset')
+__all__ = ('Container', 'Artifact', 'ActionRun', 'Asset', 'App')
---
+++ pyphantomapi/models/phantom/app.py
@@ -0,0 +1,6 @@
+from .base import PhantomBase
+
+
+class App(PhantomBase):
+    def __init__(self, phantom, _data):
+        super().__init__(phantom, _data)
